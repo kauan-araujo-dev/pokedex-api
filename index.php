f2eba75adce7b6d6b3fe6361d786e10e16256226
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-10-21 14:03:37 -0300

    estilização da pokedex

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once("functions.php");
 
-$pokemon;
+$pokemon = "";
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $pokemon = $_POST['pokemon'];
 }
@@ -12,21 +12,29 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Pokedex</title>
 </head>
 <body>
+    <h1>Pokédex</h1>
+    <div id="container-pokedex">
     <form action="" method="post">
-        <label for="pokemon">ESCOLHA UM POKEMON</label>
+       <div>
+         <label for="pokemon">ESCOLHA UM POKEMON</label>
         </label>
          <select name="pokemon" id="pokemon">
                 <option value=""></option>
                 <?= optionsPokemon(); ?>
             </select>
+       </div>
             <input type="submit" value="BUSCAR POKEMON">
     </form>
 
-    <div>
-        <?=printInfoPokemon($pokemon)?>
+    <div id="info-pokemon">
+        <?php if($pokemon !== ""){
+            echo printInfoPokemon($pokemon);
+        }?>
+    </div>
     </div>
 </body>
 </html>
